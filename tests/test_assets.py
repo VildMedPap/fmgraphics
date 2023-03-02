0e0fcdac33c80b0db3ad41b0aa931da65ae1a2a7
Sebastian Steenssøe <vildmedpap@gmail.com>
2023-03-02 09:04:46 +0100

    test: add base class for testing assets

--- tests/test_assets.py
+++ tests/test_assets.py
@@ -1,63 +1,80 @@
 from pathlib import Path
 
 from fmgraphics.assets import Face
+from fmgraphics.image import Image
 import lxml.etree
 
 
-class TestFace:
+class BaseAssetTest:
+    cls: Image
+    key: str
+    path: Path
+    asset: str
+    xml_path: str
+    repr_: str
+
     def test_init(self):
         # Given
-        filepath = Path("./123.png")
+        filepath = self.path
 
         # When
-        face = Face(filepath)
+        asset = self.cls(filepath)
 
         # Then
-        assert face.filepath == filepath
-        assert face.asset == "face"
+        assert asset.filepath == filepath
+        assert asset.asset == self.asset
 
     def test_xml_path(self):
         # Given
-        filepath = Path("./123.png")
+        filepath = self.path
 
         # When
-        face = Face(filepath)
+        asset = self.cls(filepath)
 
         # Then
-        assert face._xml_path == "graphics/pictures/person/123/portrait"
+        assert asset._xml_path == self.xml_path
 
     def test_repr(self):
         # Given
-        filepath = Path("./123.png")
+        filepath = self.path
 
         # When
-        face = Face(filepath)
+        asset = self.cls(filepath)
 
         # Then
-        assert repr(face) == "Face(key=123)"
+        assert repr(asset) == self.repr_
 
     def test_xml(self):
         # Given
-        filepath = Path("./123.png")
+        filepath = self.path
 
         # When
-        face = Face(filepath)
+        asset = self.cls(filepath)
 
         # Then
-        assert face.xml.tag == "record"
-        assert face.xml.attrib["from"] == "123"
-        assert face.xml.attrib["to"] == "graphics/pictures/person/123/portrait"
+        assert asset.xml.tag == "record"
+        assert asset.xml.attrib["from"] == self.key
+        assert asset.xml.attrib["to"] == self.xml_path
 
     def test_str(self):
         # Given
-        filepath = Path("./123.png")
+        filepath = self.path
 
         # When
-        face = Face(filepath)
-        xml_str = str(face)
+        asset = self.cls(filepath)
+        xml_str = str(asset)
         xml = lxml.etree.fromstring(xml_str)
 
         # Then
         assert xml.tag == "record"
-        assert xml.attrib["from"] == "123"
-        assert xml.attrib["to"] == "graphics/pictures/person/123/portrait"
+        assert xml.attrib["from"] == self.key
+        assert xml.attrib["to"] == self.xml_path
+
+
+class TestFace(BaseAssetTest):
+    cls = Face
+    key = "123"
+    path = Path(f"./{key}.png")
+    asset = "face"
+    xml_path = f"graphics/pictures/person/{key}/portrait"
+    repr_ = f"{asset.title()}(key={key})"
